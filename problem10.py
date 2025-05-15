12e7c2554aca0564ce03b739c77683d00f28cd20
Vincent C. Dumaguet <93762010+TRIPIXSTEVEOFFICIAL@users.noreply.github.com>
2025-05-15 09:59:28 +0800

    Delete problem10.py

--- problem10.py
+++
@@ -1,7 +0,0 @@
-print("\nVincent C. Dumaguet")
-print("Activity #10")
-print("Date 2/25/2025")
-
-words = input("Enter words separated by space: ").split()
-delimiter = input("Enter a delimiter: ")
-print("Joined String", delimiter.join(words))
