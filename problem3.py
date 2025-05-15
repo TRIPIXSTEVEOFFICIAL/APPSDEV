2d971ede3990389d441a5ba49c73074173ae2e4f
Vincent C. Dumaguet <93762010+TRIPIXSTEVEOFFICIAL@users.noreply.github.com>
2025-05-15 09:59:58 +0800

    Delete problem3.py

--- problem3.py
+++
@@ -1,7 +0,0 @@
-print("\nVincent C. Dumaguet")
-print("Activity #3")
-print("Date 2/25/2025")
-
-string = input("Enter a string: ")
-prefix = input("Enter a prefix to check: ")
-print(f"The string starts with '{prefix}':", string.startswith(prefix))
