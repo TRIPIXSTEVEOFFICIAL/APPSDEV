43d32b2df4924d8a4cdd6ea6beac29f810fba7d1
Vincent C. Dumaguet <93762010+TRIPIXSTEVEOFFICIAL@users.noreply.github.com>
2025-05-15 10:00:48 +0800

    Delete problem7.py

--- problem7.py
+++
@@ -1,7 +0,0 @@
-print("\nVincent C. Dumaguet")
-print("Activity #7")
-print("Date 2/25/2025")
-
-string = input("Enter a string: ")
-substring = input("Enter the substring to find: ")
-print(f"The substring '{substring}' is found at index {string.find(substring)}.")
